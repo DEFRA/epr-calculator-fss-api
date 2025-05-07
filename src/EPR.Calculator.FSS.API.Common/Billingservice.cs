9e910bd97d00cbbe2f8a2e211d573cc48464cc4f
udaydefra <uday.2.denduluri.external@eviden.com>
2025-05-07 13:22:02 +0100

    Adding the Essential service classes/interface

---
+++ src/EPR.Calculator.FSS.API.Common/Billingservice.cs
@@ -0,0 +1,16 @@
+﻿namespace EPR.Calculator.FSS.API.Common
+{
+    public class Billingservice : IBillingservice
+    {
+        private readonly IStorageService storageService;
+        public Billingservice(IStorageService storageService)
+        {
+            this.storageService = storageService;
+        }
+
+        public Task<string> GetBillingData(int calcRunId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/EPR.Calculator.FSS.API.Common/IBillingservice.cs
@@ -0,0 +1,12 @@
+﻿namespace EPR.Calculator.FSS.API.Common
+{
+    public interface IBillingservice
+    {
+        /// <summary>
+        /// Get the Billing File Data for the calculatorRunId
+        /// </summary>
+        /// <param name="calcRunId"></param>
+        /// <returns></returns>
+        Task<string> GetBillingData(int calcRunId);
+    }
+}
