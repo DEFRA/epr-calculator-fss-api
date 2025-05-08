04b2f19ab4966f26b4e799eaf937abb2def535eb
Jagdip Singh <jagdip.singh.external@eviden.com>
2025-05-08 11:40:28 +0100

    Add unit tests and update dockerfile

---
+++ src/EPR.Calculator.FSS.API.UnitTests/Constants/BlobStorageSettingsTests.cs
@@ -0,0 +1,58 @@
+﻿using AutoFixture;
+using EPR.Calculator.FSS.API.Constants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPR.Calculator.FSS.API.UnitTests.Constants
+{        
+    [TestClass]
+    public class BlobStorageSettingsTests
+    {
+        private BlobStorageSettings TestClass;
+        private IFixture Fixture;
+
+        public BlobStorageSettingsTests()
+        {
+            Fixture = new Fixture();
+            this.TestClass = new BlobStorageSettings();
+        }
+
+        [TestMethod]
+        public void CanSetAndGetConnectionString()
+        {
+            // Arrange
+            var testValue = Fixture.Create<string>();
+
+            // Act
+            this.TestClass.ConnectionString = testValue;
+
+            // Assert
+            Assert.AreEqual(testValue, this.TestClass.ConnectionString);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetContainerName()
+        {
+            // Arrange
+            var testValue = Fixture.Create<string>();
+
+            // Act
+            this.TestClass.ContainerName = testValue;
+
+            // Assert
+            Assert.AreEqual(testValue, this.TestClass.ContainerName);
+        }
+
+        [TestMethod]
+        public void CanSetAndGetCsvFileName()
+        {
+            // Arrange
+            var testValue = Fixture.Create<string>();
+
+            // Act
+            this.TestClass.CsvFileName = testValue;
+
+            // Assert
+            Assert.AreEqual(testValue, this.TestClass.CsvFileName);
+        }
+    }
+}
