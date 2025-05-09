a5f586ebe7270e80b9367c138bc0126c245110f2
Tim Powell <timothy.powell@eviden.com>
2025-05-09 16:01:38 +0100

    Add validation and telemetry added to billing controller

---
+++ src/EPR.Calculator.FSS.API.Common/Validators/RunIdValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.FSS.API.Common.Validators
+{
+    public class RunIdValidator : AbstractValidator<int>
+    {
+        public RunIdValidator()
+        {
+            RuleFor(runId => runId)
+                .NotEmpty()
+                .WithMessage("Run ID cannot be empty.")
+                .GreaterThan(0)
+                .WithMessage("Run ID must be greater than 0.");
+        }
+    }
+}
--- src/EPR.Calculator.FSS.API/Controllers/BillingController.cs
+++ src/EPR.Calculator.FSS.API/Controllers/BillingController.cs
@@ -1,23 +1,52 @@
 ﻿using EPR.Calculator.FSS.API.Common;
+using EPR.Calculator.FSS.API.Common.Validators;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Calculator.FSS.API.Controllers
 {
+    /// <summary>
+    /// Controller for the API to retrieve billings files.
+    /// </summary>
+    /// <param name="billingService">A service object that implements <see cref="IBillingservice"/>.</param>
+    /// <param name="telemetryClient">A <see cref="TelemetryClient"/>.</param>
+    /// <param name="runIdValidator">A validator for the run ID.</param>
     [Route("api/[controller]")]
-    public class BillingController
+    public class BillingController(
+        IBillingservice billingService,
+        TelemetryClient telemetryClient,
+        RunIdValidator runIdValidator)
+        : Controller
     {
-        private readonly IBillingservice billingService;
+        private IBillingservice BillingService { get; init; } = billingService;
 
-        public BillingController(IBillingservice billingService)
-        {
-            this.billingService = billingService;
-        }
+        private RunIdValidator RunIdValidator { get; init; } = runIdValidator;
 
+        private TelemetryClient TelemetryClient { get; init; } = telemetryClient;
+
+        /// <summary>
+        /// API endpoint to retrieve billing details for a given runId.
+        /// </summary>
+        /// <param name="runId">The run ID to retrieve the billings details for.</param>
+        /// <returns>The billings details as a string.</returns>
         [HttpGet]
         [Route("billingDetails")]
-        public async Task<string> GetBillingsDetails(int runId)
+        public async Task<ActionResult<string>> GetBillingsDetails(int runId)
         {
-            return await this.billingService.GetBillingData(runId);
+            var validationResult = this.RunIdValidator.Validate(runId);
+            if (!validationResult.IsValid)
+            {
+                this.TelemetryClient.TrackTrace($"Billing data not found for runId: {runId}.");
+                return this.NotFound();
+            }
+
+            var billingData = await this.BillingService.GetBillingData(runId);
+
+            this.TelemetryClient.TrackTrace($"Billing data retrieved for runId: {runId} " +
+                $"at {DateTime.Now}, " +
+                $"length {billingData.Length}.");
+
+            return billingData;
         }
     }
 }
