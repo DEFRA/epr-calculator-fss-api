157ab5bcdf68e96c14a3b713d4075f63a9aba165
Abid Kayani <abid.kayani.external@eviden.com>
2025-05-13 03:19:06 +0100

    547163: Validation updates

--- src/EPR.Calculator.FSS.API/Controllers/OrganisationsController.cs
+++ src/EPR.Calculator.FSS.API/Controllers/OrganisationsController.cs
@@ -2,23 +2,38 @@
 
 using EPR.Calculator.FSS.API.Common.Models;
 using EPR.Calculator.FSS.API.Common.Services;
+using EPR.Calculator.FSS.API.Common.Validators;
 using EPR.Calculator.FSS.API.Shared;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
+using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
 
+/// <summary>
+/// Controller for the API to retrieve Organization Details.
+/// </summary>
 [ApiController]
 [Route("organisations-details")]
 public class OrganisationsController : ControllerBase
 {
     private readonly IOrganisationService _organisationService;
     private readonly ILogger<OrganisationsController> _logger;
+    private AbstractValidator<OrganisationSearchFilter> _organisationSearchFilterValidator;
 
-    public OrganisationsController(IOrganisationService organisationService, ILogger<OrganisationsController> logger)
+    public OrganisationsController(
+        IOrganisationService organisationService,
+        AbstractValidator<OrganisationSearchFilter> organisationSearchFilterValidator,
+        ILogger<OrganisationsController> logger)
     {
         _organisationService = organisationService;
         _logger = logger;
+        _organisationSearchFilterValidator = organisationSearchFilterValidator;
     }
 
     [HttpGet]
@@ -29,15 +44,22 @@ public class OrganisationsController : ControllerBase
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetOrganisationsDetails([FromQuery] string? createdOrModifiedAfter)
     {
-        if (createdOrModifiedAfter == null)
+        if (createdOrModifiedAfter != null)
         {
-            return HandleError.HandleErrorWithStatusCode(HttpStatusCode.BadRequest);
+            // validator for createdOrModifiedAfter - should be a valid date when not null
+            // Optional date parameter in ISO 8601 format(YYYY - MM - DD).If no date is passed, all organization records are returned.
+            // If a date is passed, only records created or modified on or after that date are returned.
+            OrganisationSearchFilter? orgSerach = new OrganisationSearchFilter();
+            orgSerach = new OrganisationSearchFilter() { CreatedOrModifiedAfter = createdOrModifiedAfter };
+            var result = this._organisationSearchFilterValidator.Validate(orgSerach);
+            if (!result.IsValid)
+            {
+                return HandleError.HandleErrorWithStatusCode(HttpStatusCode.BadRequest);
+            }
         }
 
         try
         {
-            //TODO: Add validator for createdOrModifiedAfter - should be a valid date when not null
-
             var organisationList = await _organisationService.GetOrganisationsDetails(createdOrModifiedAfter);
             if (organisationList == null)
             {
--- src/EPR.Calculator.FSS.API/Validators/OrganisationSearchFilterValidator.cs
+++ src/EPR.Calculator.FSS.API/Validators/OrganisationSearchFilterValidator.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Globalization;
 using EPR.Calculator.FSS.API.Common.Models;
 using FluentValidation;
 using FluentValidation.Results;
 
 namespace EPR.Calculator.FSS.API.Common.Validators;
+
 public class OrganisationSearchFilterValidator : AbstractValidator<OrganisationSearchFilter>
 {
     public OrganisationSearchFilterValidator()
@@ -13,9 +15,15 @@ public class OrganisationSearchFilterValidator : AbstractValidator<OrganisationS
         RuleFor(x => x.CreatedOrModifiedAfter).Must(BeAValidDate).WithMessage("Please enter a valid date. E.g. 2025-05-20");
     }
 
-    private static bool BeAValidDate(string postcode)
+    private static bool BeAValidDate(string createdOrModifiedAfter)
     {
-        // custom date validating logic goes here
-        return true;
+        string[] formats = { "yyyy/MM/yyyy", "yyyy/MMM/dd" };
+        var validDate = DateTime.TryParseExact(createdOrModifiedAfter, formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+        return validDate;
     }
+
+    /*public static IRuleBuilderOptions<T, string> MustBeDateTimeOfFormat<T>(this IRuleBuilder<T, string> ruleBuilder, string format)
+    {
+        return ruleBuilder.Must(x => DateTime.TryParseExact(x, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out _));
+    }*/
 }
