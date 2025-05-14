535548d148614e3569cb3d154c02e0c5d77e1da2
Abid Kayani <abid.kayani.external@eviden.com>
2025-05-14 02:24:09 +0100

    547163: Date Validation unit tests

--- src/EPR.Calculator.FSS.API/Validators/OrganisationSearchFilterValidator.cs
+++ src/EPR.Calculator.FSS.API/Validators/OrganisationSearchFilterValidator.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using EPR.Calculator.FSS.API.Common.Models;
 using FluentValidation;
-using FluentValidation.Results;
 
 namespace EPR.Calculator.FSS.API.Common.Validators;
 
