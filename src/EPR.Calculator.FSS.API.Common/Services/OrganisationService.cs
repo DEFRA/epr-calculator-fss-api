f999c6369249060a13f59e1a25c3309c7a995b95
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-05-14 15:06:14 +0100

    Refactor conversion of flat db data into list

--- src/EPR.Calculator.FSS.API.Common/Services/OrganisationService.cs
+++ src/EPR.Calculator.FSS.API.Common/Services/OrganisationService.cs
@@ -6,6 +6,7 @@ using EPR.Calculator.FSS.API.Common.Models;
 using EPR.Calculator.FSS.API.Common.Services;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Data;
 
 public class OrganisationService : IOrganisationService
@@ -22,6 +23,8 @@ public class OrganisationService : IOrganisationService
 
     public async Task<IReadOnlyCollection<OrganisationDetails>> GetOrganisationsDetails(string? createdOrModifiedAfter = null)
     {
+        var organisationsList = new List<OrganisationDetails>();
+
         try
         {
             const string sql = "EXECUTE [dbo].[GetLatestAcceptedGrantedOrgData] @createdOrModifiedAfter";
@@ -33,75 +36,56 @@ public class OrganisationService : IOrganisationService
 
             var dbResponse = await _synapseDbContext.RunSqlAsync<AcceptedGrantedOrgDataResponseModel>(sql, parameters);
 
-            var groupedResults = dbResponse?
-                .Where(x => x.OrganisationId is not null)
-                .GroupBy(x => x.OrganisationId);
+            var organisationsLookup = dbResponse
+               .Where(x => x.OrganisationId is not null)
+               .ToLookup(x => x.OrganisationId!.Value);
 
-            var resultList = new List<OrganisationDetails>();
-
-            if (groupedResults is not null)
+            foreach (var key in organisationsLookup.Select(x => x.Key))
             {
-                foreach (var group in groupedResults)
+                var parent = organisationsLookup[key].First();
+
+                var o = new OrganisationDetails
                 {
-                    // Should have one item with no subsidiary
-                    var items = group.OrderBy(g => g.SubsidiaryId);
-
-                    // First one should have the parent with a null subsidiary id
-                    var parent = items
-                        .FirstOrDefault(x => x.SubsidiaryId is null);
-
-                    if (parent is null)
-                    {
-                        _logger.LogWarning("No parent item found for organisation {OrganisationId}", group.Key);
-                        continue;
-                    }
-
-                    var parentOrganisation = new OrganisationDetails
-                    {
-                        OrganisationId = parent.OrganisationId.ToString() ?? string.Empty,
-                        OrganisationName = parent.OrganisationName,
-                        OrganisationTradingName = parent.TradingName,
-                        CompaniesHouseNumber = parent.CompaniesHouseNumber,
-                        HomeNationCode = parent.HomeNationCode,
-                        ServiceOfNoticeAddrLine1 = parent.ServiceOfNoticeAddrLine1,
-                        ServiceOfNoticeAddrLine2 = parent.ServiceOfNoticeAddrLine2,
-                        ServiceOfNoticeAddrCity = parent.ServiceOfNoticeAddrCity,
-                        ServiceOfNoticeAddrCounty = parent.ServiceOfNoticeAddrCounty,
-                        ServiceOfNoticeAddrCountry = parent.ServiceOfNoticeAddrCountry,
-                        ServiceOfNoticeAddrPostcode = parent.ServiceOfNoticeAddrPostcode,
-                        ServiceOfNoticeAddrPhoneNumber = parent.ServiceOfNoticeAddrPhoneNumber,
-                        SoleTraderFirstName = parent.SoleTraderFirstName,
-                        SoleTraderLastName = parent.SoleTraderLastName,
-                        SoleTraderPhoneNumber = parent.SoleTraderPhoneNumber,
-                        SoleTraderEmail = parent.SoleTraderEmail,
-                        PrimaryContactPersonFirstName = parent.PrimaryContactPersonFirstName,
-                        PrimaryContactPersonLastName = parent.PrimaryContactPersonLastName,
-                        PrimaryContactPersonPhoneNumber = parent.PrimaryContactPersonPhoneNumber,
-                        PrimaryContactPersonEmail = parent.PrimaryContactPersonEmail,
-                        SubsidiaryDetails = new List<SubsidiaryDetails>(),
-                    };
-
-                    foreach (var item in items.Where(x => x.SubsidiaryId is not null))
-                    {
-                        parentOrganisation.SubsidiaryDetails.Add(new SubsidiaryDetails
+                    OrganisationId = parent.OrganisationId.ToString() ?? string.Empty,
+                    OrganisationName = parent.OrganisationName,
+                    OrganisationTradingName = parent.TradingName,
+                    CompaniesHouseNumber = parent.CompaniesHouseNumber,
+                    HomeNationCode = parent.HomeNationCode,
+                    ServiceOfNoticeAddrLine1 = parent.ServiceOfNoticeAddrLine1,
+                    ServiceOfNoticeAddrLine2 = parent.ServiceOfNoticeAddrLine2,
+                    ServiceOfNoticeAddrCity = parent.ServiceOfNoticeAddrCity,
+                    ServiceOfNoticeAddrCounty = parent.ServiceOfNoticeAddrCounty,
+                    ServiceOfNoticeAddrCountry = parent.ServiceOfNoticeAddrCountry,
+                    ServiceOfNoticeAddrPostcode = parent.ServiceOfNoticeAddrPostcode,
+                    ServiceOfNoticeAddrPhoneNumber = parent.ServiceOfNoticeAddrPhoneNumber,
+                    SoleTraderFirstName = parent.SoleTraderFirstName,
+                    SoleTraderLastName = parent.SoleTraderLastName,
+                    SoleTraderPhoneNumber = parent.SoleTraderPhoneNumber,
+                    SoleTraderEmail = parent.SoleTraderEmail,
+                    PrimaryContactPersonFirstName = parent.PrimaryContactPersonFirstName,
+                    PrimaryContactPersonLastName = parent.PrimaryContactPersonLastName,
+                    PrimaryContactPersonPhoneNumber = parent.PrimaryContactPersonPhoneNumber,
+                    PrimaryContactPersonEmail = parent.PrimaryContactPersonEmail,
+                    SubsidiaryDetails = organisationsLookup[key]
+                        .Where(x => x.SubsidiaryId is not null)
+                        .Select(s => new SubsidiaryDetails
                         {
-                            SubsidiaryId = item.SubsidiaryId!,
-                            SubsidiaryName = item.OrganisationName,
-                            SubsidiaryTradingName = item.TradingName,
-                        });
-                    }
+                            SubsidiaryId = s.SubsidiaryId!,
+                            SubsidiaryName = s.OrganisationName,
+                            SubsidiaryTradingName = s.TradingName,
+                        })
+                        .ToList(),
+                };
 
-                    resultList.Add(parentOrganisation);
-                }
+                organisationsList.Add(o);
             }
-
-            return resultList.AsReadOnly();
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "Error occurred in GetOrganisationsDetails method. From: {CreatedOrModifiedAfter}", createdOrModifiedAfter);
-
             throw;
         }
+
+        return organisationsList.AsReadOnly();
     }
 }
